4f62db2f5145f28d1f1f0bf33b97e5080e4742ef
SiyingHuang <460335341@qq.com>
2019-07-29 23:48:09 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+import csv
+
+csv_path = r'E:\学习\充电\Python学习\流失用户分析\test1.csv'
+csv_file = csv.reader(open(csv_path,'r'))
+print(csv_file)
+
+data_ls = []
+for line in csv_file:
+    data_ls.append(line)
+
+def count_active(data):
+    counts = {}
+    for line in data_ls:
+        for x in line[1]:
+            counts[x]
