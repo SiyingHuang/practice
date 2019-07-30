0bb5e55e49c5204290c5bfcb26a8b9769b0131d4
SiyingHuang <460335341@qq.com>
2019-07-30 16:54:18 +0800

    2019.07.30 16:54

--- test1.py
+++ test1.py
@@ -1,20 +1,16 @@
-import csv
 import pandas as pd
-import collections
 
 path_home = r'E:\学习\充电\Python学习\流失用户分析\test1.csv'
 path_comp = r'D:\中移互联网\01 - 运营室\01 - 分析组\05 - 充电\Python\流失用户分析\test1.csv'
 csv_path = path_home
 csv_path = path_comp
 
-data_ls = pd.read_csv(path_comp)
+data_read = pd.DataFrame(pd.read_csv(path_comp)[:10])
 
-csv_file = csv.reader(open(csv_path,'r'))
-print(csv_file)
+pd = pd.DataFrame()
 
-def count_active(data_ls):
-    counts = collections.defaultdict(int)
-    for line in data_ls:
-        for x in line[1]:
-            counts[line[0]] += 1
-    return counts
+tmp = pd.DataFrame({'mobileno': list('aaabbb'),
+                    'date': list('cdedef'),
+                    'value': [1, 1, 1, 1, 1, 1]})
+tmp2 = tmp.set_index(['mobileno', 'date'])
+tmp3 = tmp2.unstack()
