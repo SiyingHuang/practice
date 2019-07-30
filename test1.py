286d6654ec27c978468e5957f7b13d938805df50
SiyingHuang <460335341@qq.com>
2019-07-30 13:08:31 +0800

    2019.07.30 13:08

--- test1.py
+++ test1.py
@@ -1,15 +1,20 @@
 import csv
+import pandas as pd
+import collections
 
-csv_path = r'E:\学习\充电\Python学习\流失用户分析\test1.csv'
+path_home = r'E:\学习\充电\Python学习\流失用户分析\test1.csv'
+path_comp = r'D:\中移互联网\01 - 运营室\01 - 分析组\05 - 充电\Python\流失用户分析\test1.csv'
+csv_path = path_home
+csv_path = path_comp
+
+data_ls = pd.read_csv(path_comp)
+
 csv_file = csv.reader(open(csv_path,'r'))
 print(csv_file)
 
-data_ls = []
-for line in csv_file:
-    data_ls.append(line)
-
-def count_active(data):
-    counts = {}
+def count_active(data_ls):
+    counts = collections.defaultdict(int)
     for line in data_ls:
         for x in line[1]:
-            counts[] += 1
+            counts[line[0]] += 1
+    return counts
