abca0f95ed683678ef2ecb833229f02b1bc4a1ab
SiyingHuang <460335341@qq.com>
2019-07-31 18:12:42 +0800

    2019.07.31 18:12

---
+++ liushi.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+path_home = r'E:\学习\充电\Python学习\流失用户分析\test1.csv'
+csv_path = path_home
+
+path_comp = r'D:\中移互联网\01 - 运营室\01 - 分析组\05 - 充电\Python\流失用户分析\test1.csv'
+csv_path = path_comp
+
+data = pd.DataFrame(pd.read_csv(csv_path, header=None, names=list(('mobileno','active_date','if_p_2_p')))[:10])
+data['if_active'] = 1
+
+#交换列位置-法1
+cols = list(data)
+cols.insert(2,cols.pop(cols.index('if_active')))
+data = data.loc[:,cols]
+#交换列位置-法2
+data = data[list(('mobileno', 'active_date', 'if_active'))]
+
+data_ls = data[list(('mobileno', 'active_date', 'if_active'))]
+data_ls2 = data_ls.set_index(['mobileno', 'active_date']).unstack().stack()
+
+
+
+
+
+data_msg = data[list(('mobileno','active_date','if_p_2_p'))]
