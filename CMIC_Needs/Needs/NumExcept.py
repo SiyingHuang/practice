d0e7eae7f718ff51ea9b8587643351b21e64a403
SiyingHuang <460335341@qq.com>
2019-08-12 20:46:38 +0800

    2019.08.12 20:46

--- CMIC_Needs/Needs/NumExcept.py
+++ CMIC_Needs/Needs/NumExcept.py
@@ -1,18 +1,28 @@
 import pandas as pd
+import numpy as np
 
-data_num_except = pd.read_csv(r'C:\Users\Administrator\Desktop\各品牌流失用户数据\其他\其他.txt',
+data_num_except = pd.read_csv(r'C:\Users\Administrator\Desktop\一众\华为_2-4_tichu.txt',
                       sep='|', header=None,
                       names=['mobileno'],
                       encoding='GBK')
+
 data_num_mingan = pd.read_csv(r'C:\Users\Administrator\Desktop\敏感号码.txt',
                       sep='|', header=None,
                       names=['mobileno'])
 data_num_mingan['tag'] = 1
 
-Result = pd.merge(data_num_except, data_num_mingan,
+data_num_jituan = pd.read_csv(r'C:\Users\Administrator\Desktop\中国移动集团号码及组织树.txt',
+                      sep='|', header=None,
+                      names=['mobileno'])
+data_num_jituan = data_num_jituan['mobileno'].map(lambda x: str(x)[-11:-1])
+data_num_jituan = pd.DataFrame(data_num_jituan)
+data_num_jituan['mobileno'] = data_num_jituan['mobileno'].astype('int')
+data_num_jituan['tag'] = 2
+
+Result = pd.merge(data_num_except, data_num_jituan,
                   how='left',
                   on='mobileno')
-Result = Result.loc[Result['tag'] != 1]
+Result = Result.loc[Result['tag'] != 2]
 
 def My_to_csv(data_ys, csv_name):
     data = data_ys[['mobileno']]
--- CMIC_Needs/Needs/Yuyi_NativeMsgPaaS.py
+++ CMIC_Needs/Needs/Yuyi_NativeMsgPaaS.py
@@ -14,9 +14,10 @@ data_yy = pd.read_csv(r'C:\Users\Administrator\Desktop\native_msg_07_middle.txt'
 # 【筛选符合消息量条件的号码】
 data_yy_tmp = data_yy.loc[(data_yy['all'] >= 8) & (data_yy['all'] <= 23)]
 data_yy_tmp = data_yy.loc[data_yy['all'] >= 24]
-data_yy_tmp = data_yy.loc[data_yy['fmt'] > 1]
+data_yy_tmp = data_yy.loc[data_yy['fmt'] >= 1]
+data_yy_tmp = data_yy.loc[(data_yy['all'] >= 24) | (data_yy['fmt'] >= 1)]
 
-data_yy_tmp[['mobileno']].to_csv(r'C:\Users\Administrator\Desktop\fmsg_over0.txt',
+data_yy_tmp[['mobileno']].to_csv(r'C:\Users\Administrator\Desktop\fmsg.txt',
                                  header=False, index=False)
 
 # 【剔除异常号码】
