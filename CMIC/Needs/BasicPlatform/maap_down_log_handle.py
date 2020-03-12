035813eab2a5b24cf05bd7390c93f5b42481650e
SiyingHuang <460335341@qq.com>
2020-03-12 21:49:11 +0800

    2020.03.12 21:48

--- CMIC/Needs/BasicPlatform/maap_down_log_handle.py
+++ CMIC/Needs/BasicPlatform/maap_down_log_handle.py
@@ -18,12 +18,82 @@ def maap_down_log():
     FILE = r'C:\Users\Administrator\Desktop\关于消息中台日志规范输出的需求\规范日志示例\data\maapdown\maapdown_demo.txt'
     l = []
     data = pd.read_csv(FILE, header=None, sep=',,,')
+    st = time.time()
     for i in data.index:
         line = data.iloc[i][0]
-        time = datetime.strptime(line.split(',')[0][:-4], '%Y-%m-%d %H:%M:%S')
-        main = re.search(r'sip[:](.*?)[.]com', line)
-        called = re.search(r'[+86](.*){13}', line).group()
-        msg_id = re.search(r'maapBody[:](.){36}', line).group()
+        mp_time = pd.Series(datetime.strptime(line.split(',')[0][:-4], '%Y-%m-%d %H:%M:%S'))
+        main = pd.Series(re.search(r'sip[:](.*?)[.]com', line).group().lstrip("sip:").rstrip(
+            "@botplatform.rcs.chinamobile.com"))  # “.*?”匹配尽量少（?）的字符，非贪婪（一行中存在两个sip）
+        called = pd.Series(re.search(r'[+86]\d{13}', line).group().lstrip("+86"))  # “\d”匹配任何十进制数(0-9)，共13个数字（含"+86"）
+        msg_id = pd.Series(re.search(r'maapBody[:](.){36}', line).group().lstrip("maapBody:"))  # msg_id长度为36
+        code = pd.Series(re.search(r'code[:](.){1,3}', line).group().lstrip("code:"))  # “code:”后的数字可能为1-3个
+        line_s = pd.concat((mp_time, main, called, msg_id, code), axis=1)
+        l = l.append(line_s)  # 因为line_s是DataFrame，所以要写成“l = l.append(xxx)”
+    print('本次耗时{:.1f}秒'.format(time.time() - st))
+
+def maap_down_log():
+    FILE = r'C:\Users\Administrator\Desktop\关于消息中台日志规范输出的需求\规范日志示例\data\maapdown\maapdown_demo.txt'
+    l = []
+    data = pd.read_csv(FILE, header=None, sep=',,,')
+    st = time.time()
+    mp_time_l = []
+    main_l = []
+    called_l = []
+    msg_id_l = []
+    code_l = []
+    for i in data.index:
+
+        line = data.iloc[i][0]
+        mp_time = datetime.strptime(line.split(',')[0][:-4], '%Y-%m-%d %H:%M:%S')
+        main = re.search(r'sip[:](.*?)[.]com', line).group().lstrip("sip:").rstrip(
+            "@botplatform.rcs.chinamobile.com")  # “.*?”匹配尽量少（?）的字符，非贪婪（一行中存在两个sip）
+        called = re.search(r'[+86]\d{13}', line).group().lstrip("+86")  # “\d”匹配任何十进制数(0-9)，共13个数字（含"+86"）
+        msg_id = re.search(r'maapBody[:](.){36}', line).group().lstrip("maapBody:")  # msg_id长度为36
+        code = re.search(r'code[:](.){1,3}', line).group().lstrip("code:")  # “code:”后的数字可能为1-3个
+
+        mp_time_l.append(mp_time)  # 因为mp_time是List，因此要写成“mp_time_l.append(xxx)”
+        main_l.append(main)
+        called_l.append(called)
+        msg_id_l.append(msg_id)
+        code_l.append(code)
+
+        line_s = pd.DataFrame((mp_time_l, main_l, called_l, msg_id_l, code_l))
+    print('本次耗时{:.1f}秒'.format(time.time() - st))
+
+
+st = time.time()
+pattern = re.compile(r'(.{19}).*?sip:(\d*).*?\+86(\d*).*?maapBody:(.*?) code:(\d*).*', re.DOTALL)
+mp_time_l, main_l, called_l, msg_id_l, code_l = [], [], [], [], []
+with open(r'C:\Users\Administrator\Desktop\关于消息中台日志规范输出的需求\规范日志示例\data\maapdown\maapdown_demo.txt', 'r', encoding='utf-8') as f:
+    for s in f:
+        gp = pattern.search(s)
+        if gp:
+            mp_time_l.append(gp.group(1))
+            main_l.append(gp.group(2))
+            called_l.append(gp.group(3))
+            msg_id_l.append(gp.group(4))
+            code_l.append(gp.group(5))
+        else:
+            mp_time_l.append(None)
+            main_l.append(None)
+            called_l.append(None)
+            msg_id_l.append(None)
+            code_l.append(None)
+print('本次耗时{:.1f}秒'.format(time.time() - st))
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.width', 100)
+test_data = pd.DataFrame({'mp_time': mp_time_l,
+                          'main': main_l,
+                          'called': called_l,
+                          'msg_id': msg_id_l,
+                          'code': code_l})
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -45,7 +115,7 @@ def maapdown(fname, fwname):
         time = seg[0]
         msg_id = re.search(r'maapBody[:](.){36}', str_demo).group()
         main = re.search(r'sip[:](.*?)com', str_demo).group()  # “.*?”匹配尽量少（?）的字符，非贪婪（一行中存在两个sip）
-        called = re.search(r'[+86]\d{13}', str_demo).group()  # “\d”匹配任何十进制数(0-9)，共13个数字（含"+86"）--python3
+        called = re.search(r'[+86]\d{13}', str_demo).group()  # “\d”匹配任何十进制数(0-9)，共13个数字（含"+86"）
         code = re.search(r'code[:](.){1,3}', str_demo).group()  # “code:”后的数字可能为1-3个
         fw.write(time)
         fw.write(',')
@@ -60,11 +130,3 @@ def maapdown(fname, fwname):
 
     f.close()
     fw.close()
-
-    s1 = pd.Series(time)
-    s2 = pd.Series(msg_id)
-    s = s1 + ',' + s2
-    l = []
-    l = pd.concat((s1, s2), axis=1)
-    l = l.append(l)
-    len(l)
