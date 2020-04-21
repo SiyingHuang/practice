5d5ab1eabc3623aac1acba14af0eb72786e9f149
SiyingHuang <460335341@qq.com>
2020-04-21 17:07:50 +0800

    2020.04.21 17:07

--- CMIC/Analysis/originLogAnalysis.py
+++ CMIC/Analysis/originLogAnalysis.py
@@ -1,54 +1,61 @@
 '''
 分APP（auser）、分端（user_agent）两个字段：{
-    和飞信：5d36b5b34e3f4601103c819c
-        iOS：AndFetion
-        Android：HFX
-    和办公：5d3fdc22dbdd7a668be5fbff
-        iOS：AndFetion
-        Android：okhttp/${project.version}
+    和飞信：'5d36b5b34e3f4601103c819c'
+        iOS：'AndFetion'
+        Android：'HFX'
+    和办公：'5d3fdc22dbdd7a668be5fbff'
+        iOS：'AndFetion'
+        Android：'okhttp/${project.version}'
 }
 '''
 
 import pandas as pd
 import numpy as np
 import os
+import time
 
-
-def dict_merge(d1, d2):
-    return d1.update(d2)
-
+pd.set_option('display.max_columns', 300)
+pd.set_option('display.width', 600)
 
 os.chdir(r'C:\Users\Administrator\Desktop')
 
 # 【匹配后日志】
 name_list = ['adate', 'type', 'main_number', 'called_number', 'msg_scene', 'content_type', 'msg_id', 'status_code',
              'msg_type', 'content', 'app_version', 'term_brand', 'termtype_or_iosversion', 'ip', 'p_day_id']
-col_type = dict.fromkeys(['type', 'main_number', 'called_number', 'msg_scene', 'status_code', 'p_day_id'], 'str')
+col_type = dict.fromkeys(
+    ['type', 'main_number', 'called_number', 'msg_scene', 'content_type', 'msg_id', 'status_code', 'msg_type',
+     'content', 'ip', 'p_day_id'], 'str')
 data = pd.read_csv(r'SELF_INNOVATE_ORIGIN_MESSAGE_TEMP_ALL.txt',
                    sep=r'@@sep',
                    names=name_list, dtype=col_type,
                    # parse_dates=[0],
                    engine='python', encoding='utf-8')
-len(data.loc[data.called_number.isna()])  # 被叫号码缺失记录
+len(data.loc[data.msg_id.isna()])  # 被叫号码缺失记录
 
 # iPhone、Android日志条数检查
-tmp3 = data.loc[data.p_day_id == '20200416']
+tmp3 = data.loc[data.p_day_id == '20200420'].copy()
 tmp3['new_brand'] = tmp3.term_brand.apply(lambda x: 'iPhone' if x == 'iPhone' else 'Android')
 tmp3.new_brand.value_counts()
 
-# 统计和飞信APP消息成功情况
+# 统计和飞信APP发送消息成功情况
 data.p_day_id.value_counts()
-tmp2 = data.loc[
-    (data.p_day_id == '20200416') &  # 日期
-    (data.type == '5d36b5b34e3f4601103c819c') &  # 和飞信
-    (data.msg_type == 'postMessage') &
-    ((data.status_code.map(lambda x: '2' in str(x)[0])) | (data.status_code.map(lambda x: '3' in str(x)[0])))]  # 成功
-len(tmp2.main_number.drop_duplicates())  # 主叫人数统计
-len(tmp2.msg_id.drop_duplicates())  # 消息条数统计
-tmp2.term_brand.value_counts()
-tmp2.groupby(by=['p_day_id'])['main_number'].count()
-len(tmp2.loc[tmp2.called_number.isna()])
+data['new_type'] = data.type.apply(type_distinguish)
+# data.drop(columns='new_type', inplace=True)
 
+# st = time.time()
+tmp2 = data.loc[
+    # (data.p_day_id == '20200419') &  # 日期
+    # (data.type == '5d36b5b34e3f4601103c819c') &  # 和飞信
+    (data.msg_type == 'postMessage') &  # 发消息
+    (data.status_code.map(lambda x: str(x).startswith(('2', '3'))))].copy()  # 成功
+# print('耗时{:.4f}秒'.format(time.time() - st))
+tmp2.groupby(by=['p_day_id', 'new_type', 'msg_scene'])['msg_id'].nunique()  # 去重统计
+tmp2.groupby(by=['p_day_id', 'new_type', 'msg_scene'])['main_number'].nunique()  # 去重统计
+tmp2.pivot_table(values=['main_number'],
+                 index=['p_day_id', 'new_type'],
+                 columns=['msg_scene'],
+                 aggfunc=pd.Series.nunique,  # 去重统计
+                 margins=True)
 
 
 
@@ -68,3 +75,17 @@ tmp = data.loc[
     (data.dtime == '20200416') & (data.type == '5d36b5b34e3f4601103c819c') & (data.member_function == 'postMessage') &
     (data.adate.isin(error_list))]
 tmp.to_csv(r'test.txt', index=False)
+
+
+# 区分和飞信、和办公、PC
+def type_distinguish(x):
+    if x == '5d36b5b34e3f4601103c819c':     # 和飞信
+        return 'hfx'
+    elif x == '5d3fdc22dbdd7a668be5fbff':   # 和办公
+        return 'hbg'
+    else:
+        return 'others'
+
+# 字典合并
+def dict_merge(d1, d2):
+    return d1.update(d2)
--- CMIC/Needs/NPS/npsResultScoreStatistics.py
+++ CMIC/Needs/NPS/npsResultScoreStatistics.py
@@ -6,6 +6,7 @@ import os
 
 pd.set_option('display.max_columns', 300)
 pd.set_option('display.width', 600)
+
 s = '[{"SurveyObjType":"3","QuestionTitle":"您有多大可能性向您的亲朋好友推荐和飞信？","Score":"10","SurveyLables":["不存在无法发送或接收不成功消息的情况"]},{"SurveyObjType":"3","QuestionTitle":"结合您在和飞信发送在线消息的情况，请您对和飞信的在线消息发送体验进行整体评价","Score":"10","SurveyLables":["消息发送正常"]}]'
 s = '[{"SurveyObjType":"0","QuestionTitle":"装机整体满意度","Score":"10"},{"SurveyObjType":"2","QuestionTitle":"上门及时性","Score":"10"},{"SurveyObjType":"3","QuestionTitle":"安装专业性","Score":"10"},{"SurveyObjType":"2","QuestionTitle":"安装人员服务","Score":"10"},{"SurveyObjType":"0","QuestionTitle":"装机完成后是否测速","Score":"0","SurveyLables":["是"]},{"SurveyObjType":"0","QuestionTitle":"您是否知晓中国移动推出的“快装快修，超时送流量”活动？","Score":"0"},{"SurveyObjType":"0","QuestionTitle":"您本次装机服务是否比约定时间延迟？","Score":"0"},{"SurveyObjType":"0","QuestionTitle":"由于本次延迟上门，您获得了多少流量赠送？","Score":"0"}]'
 
@@ -16,7 +17,7 @@ gp.group(1)
 gp.group(2)
 
 
-st = time.time()
+st = time.time()  # 计时开始
 os.chdir(r'C:\Users\Administrator\Desktop')
 data = pd.read_csv(r'nps_SurveyResult.txt', header=None, sep='|',
                    names=['mobileno', 'scene', 'content', 'date'])
