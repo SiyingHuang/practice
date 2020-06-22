f71f3fa47a7c169cf15a543113eedb7fa29a9f30
SiyingHuang <460335341@qq.com>
2020-06-22 17:04:22 +0800

    2020.06.22 17:04

--- Tools/provinceSegmentMatching.py
+++ Tools/provinceSegmentMatching.py
@@ -13,19 +13,23 @@ data_section_prov = pd.read_csv(path1, header=None,
                                 dtype={'sec': np.int32})
 # (2)号段表2 - 集团每日同步号段表
 path2 = r'D:\中移互联网\01 - 运营室\01 - 分析组\01 - 工作内容\【Native】\02 - 【提数】\基础数据\DIM_SECTION_NO_DAY_0610.txt'
-data_section_prov = pd.read_csv(path2, header=None, names=['prov', 'city', 'sec'], dtype={'sec': np.int32})
+data_section_prov = pd.read_csv(path2, header=None, names=['prov', 'city', 'sec'], dtype={'sec': np.int32})  # 提取前7位号段（读入11位号码时）
 
 # 2、原始数据读入
-data = pd.read_csv(r'C:\Users\Administrator\Desktop\hsy_tmp_byw.txt', header=None,
-                   names=['date', 'mobileno', 'cnts'], dtype={'cnts': np.int32})
+data = pd.read_csv(r'C:\Users\Administrator\Desktop\yy_mz_imei_2.txt', header=None,
+                   names=['mobileno', 'imei'])
+data = data.loc[data.mobileno.notna()]  # 剔除空号码
+data = data.loc[data.imei.notna()]  # 剔除空IMEI
+data = data.loc[data.mobileno.map(lambda x: len(str(x)) == 14)]  # 剔除非手机号
+data['sec'] = data.mobileno.map(lambda x: str(x)[:7]).astype(np.int32)
 
 # 3、匹配省份地市信息
 data_tmp = pd.merge(data, data_section_prov,
-                    how='inner',
+                    how='left',
                     on='sec')
-data = data.loc[data.mobileno.notna()]  # 剔除空号码
-data = data.loc[data.mobileno.map(lambda x: len(str(x)) == 14)]  # 剔除非手机号
-# data['sec'] = data['mobileno'].map(lambda x: str(x)[3:10]).astype(np.int32)  # 提取前7位号段（读入11位号码时）
+data_tmp = data_tmp.iloc[:, [0, 1, 3]]
+data_tmp.mobileno.drop_duplicates()
+data_tmp.to_csv(r'C:\Users\Administrator\Desktop\魅族周期内活跃用户IMEI明细.txt', header=None, index=False)
 # (1) 本异网条数统计
 tmp1 = data_tmp.pivot_table(values='cnts',
                             columns=['operator'],
