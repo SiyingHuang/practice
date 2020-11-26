104decaefdea3206a9833bd21b6a91e02466996a
SiyingHuang <460335341@qq.com>
2020-11-26 17:14:44 +0800

    2020.11.26 17:14

--- CMIC/Needs/Lingshu_5G_message_log.py
+++ CMIC/Needs/Lingshu_5G_message_log.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 import time
 import os
-os.chdir(r'D:\中移互联网\01 - 运营室\01 - 分析组\01 - 工作内容\【5G消息】')
+os.chdir(r'D:\中移互联网\01 - 运营室\01 - 分析组\01 - 工作内容\【5G消息】\HN20201126')
 
 
 # 测试
@@ -18,16 +18,14 @@ gp = re.search(pattern, s)
 gp.groups()
 
 
-# 分块读取处理
-
-
+# 1、分块读取处理：筛选出符合条件日志
 st = time.time()
 
-log_09 = pd.read_csv(r'HN20201113\合并\UP.txt',
+log_up = pd.read_csv(r'UP.txt',
                      header=None, sep='sep@@', names=['ua'],
                      engine='python', chunksize=100000)  # 分块读取原始日志
 chatbot_data = []
-for i in log_09:
+for i in log_up:
     tmp = i.loc[(i['ua'].map(lambda x: 'UP_2.4' in x)) & (i['ua'].map(lambda x: 'Meiz' not in x)) & (
         i['ua'].map(lambda x: 'GB' not in x))]
     chatbot_data.append(tmp)
@@ -35,6 +33,8 @@ chatbot_data = pd.concat(chatbot_data, ignore_index=True)
 
 print('耗时{:.4f}秒'.format(time.time() - st))
 
+
+pattern = re.compile(r'UP_2.4 term-(.*)/(.*) client.*sip:\+86(\d*)@(.*?)\.')
 
 def extract_brand(s):
     gp = re.search(pattern, s)
@@ -68,6 +68,7 @@ def extract_prov(s):
         return 'else'
 
 
+# 2、提取所需信息：品牌、机型、号码、省份
 st = time.time()
 result = pd.DataFrame(columns=['brand', 'term', 'mobileno', 'code'])
 result['brand'] = chatbot_data['ua'].map(extract_brand)
@@ -77,15 +78,19 @@ result['code'] = chatbot_data['ua'].map(extract_prov)
 print('耗时{:.4f}秒'.format(time.time() - st))
 
 
-chatbot_data.to_csv(r'HN20201113\符合4个条件原始日志.txt',
+chatbot_data.to_csv(r'符合3个条件原始日志.txt',
                     header=None, index=False)
-result.to_csv(r'HN20201113\result.txt',
+result.to_csv(r'符合3个条件处理后4个字段（去重）.txt',
               index=False)
-result = pd.read_csv(r'HN20201113\符合3个条件处理后4个字段.txt', dtype='str')
+# result = pd.read_csv(r'符合3个条件处理后4个字段.txt', dtype='str')
+
 result = result.drop_duplicates()  # 去重
 tmp = result.groupby(by=['code', 'brand', 'term'])['mobileno'].count()  # 去重后统计
 tmp = tmp.reset_index(drop=False)
 
-prov_code = pd.read_excel(r'5G消息各大区省份编码.xlsx', names=['code', 'prov'])  # “编码-省份”映射表
-tmp2 = pd.merge(tmp, prov_code, on='code')
+
+# 3、分省匹配
+prov_code = pd.read_excel(r'D:\中移互联网\01 - 运营室\01 - 分析组\01 - 工作内容\【5G消息】\5G消息各大区省份编码.xlsx',
+                          names=['code', 'prov'])  # “编码-省份”映射表
+tmp2 = pd.merge(tmp, prov_code, on='code')  # code字段匹配省份信息
 tmp2.to_excel(r'汇总统计.xlsx', index=False)
