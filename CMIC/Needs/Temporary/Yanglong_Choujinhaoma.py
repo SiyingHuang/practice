a7de1b0da19445006ca9db74f57ecae5dc91cea0
M.Owen <460335341@qq.com>
2021-02-24 17:17:32 +0800

    2021.02.24 17:17

--- CMIC/Needs/Temporary/Yanglong_Choujinhaoma.py
+++ CMIC/Needs/Temporary/Yanglong_Choujinhaoma.py
@@ -6,6 +6,7 @@
 """
 需求人：杨龙
 用途：
+【注】因几个需求先后提出，导致编写多段代码依次进行处理。
 1、批量处理
     各excel文件中，
     找出其中号码是否存在“不符合结算的连续号码”文件中，
@@ -15,12 +16,16 @@
     同一个excel中，出现重复号码（excel中可能有多个sheet）
     找出单个excel文件中的重复号码，
     并新建一列进行标注。
+3、excel中部分号码属于异常号段。
+    异常号段：'1719615', '1719706', '1719535', '1455663', '1455664', '1718591', '1702462', '1719249', '1719813', '1701868'
+    新建一列进行标注。
 """
 
 import numpy as np
 import pandas as pd
 import os
 import time
+import functools
 
 
 """初始化"""
@@ -29,6 +34,8 @@ result_path = r'C:\Users\M.Owen\Desktop\附件1：反馈给市场部的明细数
 conti_numbers = pd.read_csv(r'C:\Users\M.Owen\Desktop\附件2：连号号码明细out20210113numberinsamewannum-ppcg1301.txt',
                             header=None, names=['mobileno'], dtype=np.int64)  # 连号号码读取
 conti_numbers['是否连续号码'] = '是'
+
+section_numbers = pd.read_csv(r'C:\Users\M.Owen\Desktop\号段号码数.txt')
 
 
 """批量处理"""
@@ -149,7 +156,11 @@ for file in file_list:
 print('全部处理完成!\n耗时{:.4f}秒'.format(time.time() - st))
 
 
-"""判断号码是否在特定号段中"""
+"""
+判断号码是否在特定号段中
+或
+匹配该号段号码总数
+"""
 path = r'C:\Users\M.Owen\Desktop\附件1：反馈给市场部的明细数据\处理后\是否在特定号段'
 os.chdir(path)
 file_list = os.listdir(path)  # 待处理文件路径
@@ -170,10 +181,17 @@ for file in file_list:
     for sheet in sheet_list:
         excel_data = pd.read_excel(file, sheet_name=sheet)
 
-        excel_data.loc[excel_data['用户标识'].map(
-            lambda x: str(x)[:7] in ['1719615', '1719706', '1719535', '1455663', '1455664',
-                                     '1718591', '1702462', '1719249', '1719813', '1701868']), '是否特定号段'] = '是'
-        excel_data.loc[excel_data['是否特定号段'] != '是', '是否特定号段'] = '否'
+        '''判断号码是否在特定号段中'''
+        # excel_data.loc[excel_data['用户标识'].map(
+        #     lambda x: str(x)[:7] in ['1719615', '1719706', '1719535', '1455663', '1455664',
+        #                              '1718591', '1702462', '1719249', '1719813', '1701868']), '是否特定号段'] = '是'
+        # excel_data.loc[excel_data['是否特定号段'] != '是', '是否特定号段'] = '否'
+
+        '''匹配该号段号码总数'''
+        excel_data['号段'] = excel_data['用户标识'].map(lambda x: str(x)[:7]).astype(np.int64)
+        excel_data = pd.merge(excel_data, section_numbers, how='left', on='号段')
+        excel_data.loc[excel_data['号码数'].isna(), '号码数'] = '-'
+        excel_data.drop(columns=['号段'], inplace=True)
 
         excel_data.to_excel(writer, sheet_name=sheet, index=False)
 
